eb317a6eeb60866902a1921604920d5aea4294de
Chris Wallace <chriswallace00@gmail.com>
2016-05-01 11:33:01 -0400

    initial commit WITH files this time

---
+++ app.py
@@ -0,0 +1,9 @@
+# import data
+
+# def functionCalculateResult(team 1, team 2)
+# -1 = team 1 100%, +1 = team 2 100%, -0.25 - +0.25 = draw, 
+
+# work out group results
+
+# probably manual hacky method to get knock out results
+
