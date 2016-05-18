6a51b33cc041ef941f36c5178f2ba04a3b28da0d
Chris Wallace <chriswallace00@gmail.com>
2016-05-18 19:16:03 +0100

    added a simple implementation of the vader nltk sentiment analysis to get the sentiment from the last 10 google news stories about each time and adjust their ELO by up to 10xsentiment points, where -1<sentiment<1

--- analysis.py
+++ analysis.py
@@ -1,4 +1,12 @@
 import numpy as np
+import urllib2
+import socket
+from bs4 import BeautifulSoup
+from nltk import tokenize
+from nltk.classify import NaiveBayesClassifier
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class Analysis:
     def __init__(self):
@@ -35,4 +43,71 @@ class Analysis:
     # estimate prob of a draw - very rough, taken from the average of last few euro tournaments    
     def get_draw_prob(self,prob_A, prob_B):
         return -0.3556*(prob_A-prob_B)+0.3556
+    
+    # takes a team names and searches for recent news stories (last 7 days)
+    # based on the approximate sentiment, it returns an ELO adjustment
+    # a maximum of 10 point elo adjustment is pre-set as a typical maximum exchanged
+    # between higher rated teams
+    def get_news_sentiment_elo_adjustment(self,team):
+        max_elo_change=10.
+        story_limit=5 
+        neg=0
+        pos=0
+        visible_text=''
+        #negative phrases=['unfit','will not be fit','not fit','injured','injuries','depressed','disappointed','controversy','scandal','home','conceded']
+        positive_phrases=['fully fit','strong','scored']
+        negative_phrases=['unfit','injured']
+        base_url='https://www.google.co.uk/search?hl=en&gl=uk&tbm=nws&authuser=0&q=football+european+championships'+team.replace(' ','%20')
+        req = urllib2.Request(base_url,headers = {'User-Agent': 'Mozilla/5.0'} )
+        page = urllib2.urlopen(req)
+        soup = BeautifulSoup(page)
+        # remove scripts and tags
+        for script in soup(["script", "style"]):
+                script.extract()    # rip it out
+        # find the links
+        sentence_count=0
+        score=0
+        story_count=0
+        for a in soup.findAll('a'):
+            if story_count<story_limit:
+                # ignore internal google links and remove the tracking guff from the end of the URL
+                if ('google' not in a.attrs['href'] and '/search?q=football+european+championships' not in a.attrs['href'] and 'http://' in a.attrs['href']):
+                    print a.attrs['href'].replace('/url?q=','').split('&')[0]
+                    story_req=urllib2.Request(a.attrs['href'].replace('/url?q=','').split('&')[0],headers={'User-Agent': 'Mozilla/5.0'})
+                    try:
+                        story_page=urllib2.urlopen(story_req,timeout=5)
+                        story_soup=BeautifulSoup(story_page)
+                        # analyse text
+                        visible_text = story_soup.getText()
+                        neg=0
+                        pos=0
+                        sentences = tokenize.sent_tokenize(visible_text)
+                        sid = SentimentIntensityAnalyzer()
+                        # for each sentence in the story, get the sentiment/polarity
+                        for sentence in sentences:
+                            ss = sid.polarity_scores(sentence)
+                            neg=neg+ss['neg']
+                            pos=pos+ss['pos']
+                        sentence_count=sentence_count+ len(sentences)
+                        score=score+pos-neg
+                        # print out a story by story sentiment for logging
+                        print 'Sentiment: ',(pos-neg)/len(sentences)
+                    except socket.timeout as e:
+                        print type(e)    
+                    except urllib2.HTTPError:
+                        print 'failed on url: ',a.attrs['href']
+                    story_count=story_count+1 
+        # calculate an average sentiment based on all of the stories
+        # return an elo_adjustment based on the max elo change and the sentiment
+        norm_sentiment_score=(pos-neg)/len(sentences)
+        return norm_sentiment_score*max_elo_change
+
+
+        norm_sentiment_score=(pos-neg)/len(visible_text)
+        
+        # do the scraping
+        # do the sentiment analysis
+        # do the normalising
+        
+        return norm_sentiment_score*max_elo_change
         
--- app.py
+++ app.py
@@ -38,16 +38,15 @@ while True:
                     teams=name.split(' v ')
                     home_team=teams[0]
                     away_team=teams[1]
-                  
-                    home_team_elo=international_ratings.get(home_team)
-                    away_team_elo=international_ratings.get(away_team)
                     
                     # catch differences between country names in ELO ratings and Betfair
                     if ('Rep' in home_team):
-                        home_team_elo =international_ratings.get('Ireland')
+                        home_team_elo =float(international_ratings.get('Ireland'))+float(analysis.get_news_sentiment_elo_adjustment('Ireland'))
                     elif ('Rep' in away_team):
-                        away_team_elo =international_ratings.get('Ireland')
-                    
+                        away_team_elo =float(international_ratings.get('Ireland'))+float(analysis.get_news_sentiment_elo_adjustment('Ireland'))
+                    else:
+                        home_team_elo=float(international_ratings.get(home_team))+float(analysis.get_news_sentiment_elo_adjustment(home_team))
+                        away_team_elo=float(international_ratings.get(away_team))+float(analysis.get_news_sentiment_elo_adjustment(away_team))
                     # order matters when calculating the ELO difference and probabilities
                     if (home_team_elo>away_team_elo):
                         probs = analysis.get_probabilities_from_elo(home_team_elo,away_team_elo)
@@ -68,6 +67,8 @@ while True:
                     database.store_analysis(str(datetime.datetime.now()),match_id,home_team,away_team,odds[0],odds[1],odds[2],(1/odds[0])/over_round,(1/odds[1])/over_round,(1/odds[2])/over_round,date,home_team_prob,away_team_prob,draw_prob,home_team_elo,away_team_elo,'normal')
                     # store the values for the current structure
                     database.store_odds(str(datetime.datetime.now()),match_id,name,odds[0],odds[1],odds[2],date)
+                    
+                 #   print home_team,": ",analysis.get_news_sentiment(home_team)
     except Warning as warn:
         print 'Warning: %s ' %warn  + '\nStop.\n'
         # wait 1 hour then run again
